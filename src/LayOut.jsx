32fc825823a4cf5de7f1a6a61936bc9c8e317a45
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-05-11 10:46:09 +0600

    Footer Completed

--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
-// import Footer from "./Footer/Footer";
+import Footer from "./Footer/Footer";
 import Nav from "./Nav/Nav";
 const LayOut = () => {
   return (
     <div className="flex flex-col min-h-[100vh] justify-between bg-base-100">
       <Nav> </Nav>
       <Outlet></Outlet>
-      {/* <Footer></Footer> */}
-      <div className="text-4xl">faf</div>
+      <Footer></Footer>
     </div>
   );
 };
